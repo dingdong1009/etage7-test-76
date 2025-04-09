b77afd540d8fa9a02a969afeed3b504130e14436
dingdong1009 <cphamtiktok@gmail.com>
2025-04-09 08:37:10 +0200

    admni bg white

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -4,7 +4,7 @@ import AdminHeader from "./AdminHeader";
 
 const AdminLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="min-h-screen flex flex-col">
       <AdminHeader />
       <main className="flex-1 p-4 md:p-6 lg:p-8">
         <Outlet />
