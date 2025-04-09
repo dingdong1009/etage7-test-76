3d8cbf0c6689f1090ee8560facc54269882dd29d
dingdong1009 <cphamtiktok@gmail.com>
2025-04-09 10:45:16 +0200

    import button sales manager users

--- src/pages/sales/Users.tsx
+++ src/pages/sales/Users.tsx
@@ -2,6 +2,7 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 const SalesUsers = () => {
   // Sample user data
