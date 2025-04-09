db945551a0b1f3572413a5bc20c6d9d2358ce058
dingdong1009 <cphamtiktok@gmail.com>
2025-04-09 08:27:14 +0200

    header index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,10 +53,10 @@ const Header = () => {
               <li key={item.name}>
                 <Link 
                   to={item.path} 
-                  className="text-sm font-light relative group"
+                  className="text-sm font-light relative group hover:text-black"
                 >
                   {item.name.toUpperCase()}
-                  <span className="absolute left-0 bottom-2 w-0 h-[1px] bg-black transition-all duration-300 group-hover:w-full"></span>
+                  <span className="absolute left-0 bottom-2 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full"></span>
                 </Link>
               </li>
             ))}
