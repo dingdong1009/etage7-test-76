3c5a9309b5b00b69985d9306a732caf4387ebb61
dingdong1009 <cphamtiktok@gmail.com>
2025-04-10 22:19:16 +0200

    test

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -29,8 +29,8 @@ const AdminHeader = () => {
   return (
     <header className="sticky top-0 left-0 right-0 z-40 bg-white">
       <div className="max-w-full px-4 flex justify-between items-center h-16">
-        <div className="flex items-center gap-2">
-        <Link to="/" className="text-black text-2xl font-bold uppercase">ETAGE7</Link> | ADMIN
+        <div className="flex items-center bg-black gap-2">
+        <Link to="/" className="text-white text-2xl font-bold uppercase">ETAGE7</Link> | ADMIN
         </div>
         
         {/* Mobile menu button */}
