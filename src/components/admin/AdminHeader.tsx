46fbd8f88f4d7db9ccb568392ff7bc18fddd0c50
dingdong1009 <cphamtiktok@gmail.com>
2025-04-11 07:01:24 +0200

    bg black

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -28,7 +28,7 @@ const AdminHeader = () => {
 
   return (
     <header className="sticky top-0 left-0 right-0 z-40 bg-white">
-      <div className="max-w-full px-4 flex justify-between items-center h-16">
+      <div className="max-w-full bg-black px-4 flex justify-between items-center h-16">
         <div className="flex items-center gap-2">
         <Link to="/" className="text-white text-2xl font-bold uppercase">ETAGE7</Link> | ADMIN
         </div>
