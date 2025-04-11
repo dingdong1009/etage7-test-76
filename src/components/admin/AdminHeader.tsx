b3cbd6aad721df98a544d4bfc7c5f750b4d8ffac
dingdong1009 <cphamtiktok@gmail.com>
2025-04-11 07:04:34 +0200

    test

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -30,7 +30,7 @@ const AdminHeader = () => {
     <header className="sticky top-0 left-0 right-0 z-40 bg-white">
       <div className="max-w-full bg-black px-4 flex justify-between items-center h-16">
         <div className="flex items-center gap-2">
-        <Link to="/" className="text-white text-2xl font-bold uppercase">ETAGE7</Link> | ADMIN
+        <Link to="/" className="text-white text-2xl font-bold uppercase">ETAGE7</Link>
         </div>
         
         {/* Mobile menu button */}
@@ -66,14 +66,15 @@ const AdminHeader = () => {
               </li>
             ))}
           </ul>
-        </div>
-                
-        {/* User options on desktop */}
+                  {/* User options on desktop */}
         <div className="hidden md:flex items-center space-x-4">
           <Link to="/" className="text-gray-600 hover:text-black text-sm">
             BACK TO SITE
           </Link>
         </div>
+        </div>
+                
+
       </nav>
       
       {/* Mobile Menu */}
