037189d81e1313ee1a3257508afb1d090e2190b1
dingdong1009 <cphamtiktok@gmail.com>
2025-04-11 09:32:33 +0200

    bg black tx white - brand header

--- src/components/brand/BrandHeader.tsx
+++ src/components/brand/BrandHeader.tsx
@@ -30,9 +30,9 @@ const BrandHeader = () => {
 
   return (
     <header className="sticky top-0 left-0 right-0 z-40 bg-white">
-      <div className="max-w-full px-4 flex justify-between items-center h-16">
+      <div className="max-w-full px-4 flex bg-black justify-between items-center h-16">
         <div className="flex items-center gap-2">
-          <Link to="/" className="text-black text-2xl font-bold uppercase">ETAGE7</Link>
+          <Link to="/" className="text-white text-2xl font-bold uppercase">ETAGE7</Link>
         </div>
         
         {/* Mobile menu button */}
