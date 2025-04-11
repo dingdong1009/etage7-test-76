beeaccd362025f4e1e5b43372c94dab7439ecdb8
dingdong1009 <cphamtiktok@gmail.com>
2025-04-11 13:01:41 +0200

    testing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 pt-16">
+      <main className="max-w-[1481px] mx-auto flex-1 pt-16">
         {children}
       </main>
       <Footer />
