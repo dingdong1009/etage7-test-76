a0c56810434f7702a3c10b378f2f317869c6cf9e
dingdong1009 <cphamtiktok@gmail.com>
2025-04-11 13:03:49 +0200

    testing

--- src/components/brand/BrandLayout.tsx
+++ src/components/brand/BrandLayout.tsx
@@ -6,7 +6,7 @@ const BrandLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <BrandHeader />
-      <main className="flex-1 p-4 md:p-6 lg:p-8">
+      <main className="max-w-[1481px] mx-auto flex-1 pt-16">
         <Outlet />
       </main>
       <footer className="py-4 px-4 md:px-6 text-center text-sm text-gray-500 border-t border-gray-200 bg-white">
