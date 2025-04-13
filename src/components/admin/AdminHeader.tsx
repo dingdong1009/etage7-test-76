b2453c0cac56bae435708708c69ba351bec7ce63
dingdong1009 <cphamtiktok@gmail.com>
2025-04-13 11:54:34 +0200

    mx-width full

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -116,8 +116,8 @@ const AdminHeader = () => {
       </div> 
       
       {/* Desktop Navigation - Below the header bar */}
-      <nav className="hidden lg:block">
-        <div className="mx-auto px-4 py-2"> 
+        <nav className="hidden lg:block">
+        <div className="max-w-full mx-auto px-4 py-2">
           <ul className="flex space-x-8 overflow-x-auto">
             {menuItems.map((item) => (
               <li key={item.name}>
