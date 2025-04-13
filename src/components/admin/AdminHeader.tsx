8977f38a728b681706cbb0d6d65800ba4afae829
dingdong1009 <cphamtiktok@gmail.com>
2025-04-13 13:18:15 +0200

    spacer

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -128,7 +128,7 @@ const AdminHeader = () => {
                         {item.name.toUpperCase()}
                         <span
                           className={`absolute left-0 bottom-[-3px] w-0 h-[1px] bg-black transition-all duration-300 group-hover:w-full ${
-                            isActive(item.path) ? "w-full" : ""
+                            isActive(item.path) ? "w-full" : "" 
                           }`}
                         ></span> 
                       </Link>
