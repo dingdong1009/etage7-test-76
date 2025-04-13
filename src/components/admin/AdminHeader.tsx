501aafb8d45f96efc43c00b4fc3e994f73ccfd06
dingdong1009 <cphamtiktok@gmail.com>
2025-04-13 11:52:44 +0200

    navigation

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -116,8 +116,8 @@ const AdminHeader = () => {
       </div> 
       
       {/* Desktop Navigation - Below the header bar */}
-      <nav className="hidden md:block bg-white">
-        <div className="max-w-[1481px] mx-auto px-4 py-2">
+      <nav className="hidden lg:block">
+        <div className="mx-auto px-4 py-2">
           <ul className="flex space-x-8 overflow-x-auto">
             {menuItems.map((item) => (
               <li key={item.name}>
