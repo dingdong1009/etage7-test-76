ef027162b7261357902d2ec1cbb8be9fa74ab3e1
dingdong1009 <cphamtiktok@gmail.com>
2025-04-14 06:38:32 +0200

    buyer layout

--- src/components/buyer/BuyerLayout.tsx
+++ src/components/buyer/BuyerLayout.tsx
@@ -5,11 +5,9 @@ import BuyerHeader from "./BuyerHeader";
 const BuyerLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <BuyerHeader />
-      <main className="flex-1 px-4 md:px-8 py-8">
-        <div className="max-w-7xl mx-auto">
-          <Outlet />
-        </div>
+      <BrandHeader />
+      <main className="flex-1 p-4 md:p-6 lg:p-8 pt-24 max-w-[1481px] mx-auto w-full animate-fade-in">
+        <Outlet />
       </main>
       <footer className="py-8 px-4 md:px-8 border-t border-gray-100 bg-white">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
