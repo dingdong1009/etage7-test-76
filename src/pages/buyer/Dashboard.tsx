1484d30422bfbece806ef16dfd6ec8d8cdebd091
dingdong1009 <cphamtiktok@gmail.com>
2025-04-14 07:02:05 +0200

    font normal

--- src/pages/buyer/Dashboard.tsx
+++ src/pages/buyer/Dashboard.tsx
@@ -101,9 +101,8 @@ const Dashboard = () => {
   
       <section className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
         <div className="max-w-7xl mx-auto">
-          <h1 className="text-3xl md:text-5xl uppercase font-light mb-6">
-            ADDITIONAL<br/>
-            <span className="font-normal">SERVICES</span>
+          <h1 className="text-3xl md:text-5xl uppercase font-normal mb-6">
+            ADDITIONAL SERVICES
           </h1>
           <p className="max-w-2xl text-lg font-light mb-8">
             Specialized consulting services to enhance your buying strategy and store curation
