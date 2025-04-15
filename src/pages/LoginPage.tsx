ab35b6683c802bac371a3d38ae3e87581e492513
dingdong1009 <cphamtiktok@gmail.com>
2025-04-15 08:04:13 +0200

    space

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -46,7 +46,7 @@ const LoginPage = () => {
     <div className="pt-16 pb-16 px-4">
       <div className="max-w-[500px] mx-auto">
         <h1 className="text-4xl md:text-5xl lg:text-7xl font-normal tracking-tighter mb-6 uppercase">
-          Sign In
+          Sign In 
         </h1>
         
         <Form {...form}>
